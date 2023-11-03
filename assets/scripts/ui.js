75ac4615a615f43439e7ea96df02a4f591fae2bc
Lubin Pappalardo <lubinpappalardo@gmail.com>
2023-11-03 18:56:07 -0400

    Mobile adaptations

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -1,3 +1,10 @@
+let isMobile;
+
+if (window.innerWidth < 1040) {
+    isMobile = true;
+}
+
+
 // Navbar
 const nav = document.getElementById('nav');
 nav.classList.add('onTop');
@@ -14,4 +21,76 @@ window.addEventListener('scroll', () => {
 // open mobile navigation
 document.getElementById('mobile-menu-btn').addEventListener('click', () => {
     document.getElementById('mobile-menu').classList.toggle('opened');
+});
+
+
+
+
+const playerInformations = document.getElementById('player-informations');
+const scrollContainer = document.querySelector('#player-informations .infos');
+let isUserScrolling = false;
+let scrollContainerCopy;
+
+// on ready
+document.addEventListener('DOMContentLoaded', () => {
+    if (isMobile) {
+        // clone scroll container and add it to the playerInformations
+        const clonedScrollContainer = scrollContainer.cloneNode(true);
+        scrollContainerCopy = playerInformations.appendChild(clonedScrollContainer);
+    }
+});
+
+// on resize
+window.addEventListener('resize', () => {
+    if (window.innerWidth < 1040) {
+        isMobile = true;
+        // check if the copy already exists
+        if (!scrollContainerCopy) {
+            const clonedScrollContainer = scrollContainer.cloneNode(true)
+            scrollContainerCopy = playerInformations.appendChild(clonedScrollContainer);
+        }
+    } else {
+        isMobile = false;
+        if (scrollContainerCopy) {
+            scrollContainerCopy.remove();
+            scrollContainerCopy = null;
+        }
+    }
+});
+
+
+// Function to stop and resume animation
+function toggleAnimation() {
+    if (isUserScrolling) {
+        scrollContainer.style.animationPlayState = 'paused';
+        scrollContainerCopy.style.animationPlayState = 'paused';
+    } else {
+        scrollContainer.style.animationPlayState = 'running';
+        scrollContainerCopy.style.animationPlayState = 'running';
+    }
+}
+
+let scrollContainerDragStart = 0;
+
+// Listen for user scroll events
+playerInformations.addEventListener('touchstart', function(e) {
+    if (isUserScrolling) {
+        isUserScrolling = false;
+    } else {
+        isUserScrolling = true;
+    }
+
+    // scrollContainerDragStart = e.touches[0].clientX;
+    toggleAnimation();
+});
+
+// Listen for touchend events
+playerInformations.addEventListener('touchend', function() {
+    if (isUserScrolling) {
+        setTimeout(function() {
+            isUserScrolling = false;
+            toggleAnimation();
+        }, 2000);
+    }
+
 });
